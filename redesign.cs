9a98985e8a1fa03a7d0050b10ac2e669e8691f3a
Brette <98058652+brette-0@users.noreply.github.com>
2023-12-15 10:13:52 +0000

    Added Tested Metatile and Untesteed Background

--- redesign.cs
+++ redesign.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.Drawing;
+using System.Security.AccessControl;
+using System.Collections.Generic;
+using static Common;
+using System.Threading.Tasks.Dataflow;
 
 public class Program {
     static void Main(string[] args) {
@@ -10,33 +14,136 @@ public class Program {
 
 public class Common {
     Tile[,] ProcessedCHR = new Tile[2, 256];
+
+    Palette[,] AreaPalettes = new Palette[4, 4];
+    Palette[,] EnemyPalettes = new Palette[4, 4];
+    Palette[] AreaStylePalettes = new Palette[3];
+    Palette BowserPalette;
+
+    Dictionary<ushort, Bitmap> Metatiles = [];
+
+    private BackgroundItem[,] BackSceneryData = new BackgroundItem[3, 48];
+    private BackgroundElement[] BackSceneryMetatiles = new BackgroundElement[12];
+
     public Common(string[] args) {
-        byte[] CHRRAW;// = new byte[0x2000];
+        byte[] CHRRAW, PalettesRAW, MetatilesRAW, SceneryRAW;
         try {
             CHRRAW = File.ReadAllBytes(args.Length == 0 ? ".CHR" : args[0]);
+            PalettesRAW = File.ReadAllBytes(args.Length == 0 ? "palettes.bin" : args[1]);
+            MetatilesRAW = File.ReadAllBytes(args.Length == 0 ? "metatiles.bin" : args[2]);
+            SceneryRAW = File.ReadAllBytes(args.Length == 0 ? "scenery.bin" : args[3]);
         } catch (Exception ex) {
             Console.WriteLine($"An error occurred: {ex.Message}");
             return;
         }
-        Console.WriteLine("Managed to read CHR");
+        
         ProcessCHR(ref ProcessedCHR, ref CHRRAW);
-        Console.WriteLine("Managed to Normalize CHR");
+        
+        void NormalizePalettes() {
+            ushort peek = 3;
+            byte[] thisPalette = new byte[4];
+            for (byte AreaType = 0; AreaType < 4; AreaType++) {
+                for (byte AreaPalette = 0; AreaPalette < 4; AreaPalette++) {
+                    for (byte PaletteColor = 0; PaletteColor < 4; PaletteColor++) {
+                        thisPalette[PaletteColor] = PalettesRAW[peek++];
+                    }
+                    AreaPalettes[AreaType, AreaPalette] = new Palette(thisPalette, NESPALETTE);
+                }
 
-        byte[] thisPalette = [0x00, 0x01, 0x02, 0x03];
-        for (ushort Tile = 0; Tile < 512; Tile++) {
-
-            Console.WriteLine(Tile + " " + ProcessedCHR[Tile >> 8, Tile & 0xff].Data[0,0]);
-            Bitmap TileBMP = new Bitmap(8, 8);
-            using (Graphics g = Graphics.FromImage(TileBMP)) {
-                g.Clear(Color.Transparent);
+                for (byte SpritePalette = 0; SpritePalette < 4; SpritePalette++) {
+                    for (byte PaletteColor = 0; PaletteColor < 4; PaletteColor++) {
+                        thisPalette[PaletteColor] = PalettesRAW[peek++];
+                    }
+                    EnemyPalettes[AreaType, SpritePalette] = new Palette(thisPalette, NESPALETTE);
+                }
+                peek += 4;
             }
-            for (byte Y = 0; Y < 8; Y++) {
-                for (byte X = 0; X < 8; X++) {
-                    TileBMP.SetPixel(X, Y, NESPALETTE[thisPalette[ProcessedCHR[Tile >> 8, Tile & 0xff].Data[Y,X]]]);
+            for (byte AreaStyle = 0; AreaStyle < 3; AreaStyle++) {
+                for (byte PaletteColor = 0; PaletteColor < 4; PaletteColor++) {
+                    thisPalette[PaletteColor] = PalettesRAW[peek++];
+                }
+                AreaStylePalettes[AreaStyle] = new Palette(thisPalette, NESPALETTE);
+                peek += 4;
+            }
+            for (byte BowserColor = 0; BowserColor < 4; BowserColor++) {
+                thisPalette[BowserColor] = PalettesRAW[peek++];
+            }
+            BowserPalette = new Palette(thisPalette, NESPALETTE);
+        };
+
+        NormalizePalettes();
+
+        void BuildMetatiles() {
+
+            Bitmap BuildMetatile(Tile[] Tiles, Palette _Palette) {
+                Bitmap Return = new Bitmap(16, 16);
+                for (byte Width = 0, _Tile = 0; Width < 2; Width++) {
+                    for (byte Height = 0; Height < 2; Height++, _Tile++) {
+                        for (byte Y = 0; Y < 8; Y++) {
+                            for (byte X = 0; X < 8; X++) {
+                                Return.SetPixel((Width * 8) + X, (Height * 8) + Y, _Palette.Data[Tiles[_Tile].Data[Y, X]]);
+                            }
+                        }
+                    }
+                }
+                return Return;
+            }
+
+            ushort peek = 0;
+            Tile[] thisMetatile = new Tile[4];
+            byte[] TileBeginnings = [0x27, 0x2E, 0x0A, 0x06];
+
+            for (byte Metatile = 0; Metatile < TileBeginnings[0]; Metatile++) {
+                for (byte _Tile = 0; _Tile < 4; _Tile++, peek++) {
+                    thisMetatile[_Tile] = GetTile(false, MetatilesRAW[peek]);
+                }
+                for (byte AreaType = 0; AreaType < 4; AreaType++) {
+                    Metatiles[(ushort)((AreaType << 8) |Metatile)] = BuildMetatile(thisMetatile, AreaPalettes[AreaType, 0]);
+                }
+
+                for (byte AreaStyle = 1; AreaStyle < 4; AreaStyle++) {
+                    Metatiles[(ushort)((AreaStyle << 10) | Metatile)] = BuildMetatile(thisMetatile, AreaStylePalettes[AreaStyle-1]);
+                }
+
+                Metatiles[(ushort)((0x1000) | Metatile)] = BuildMetatile(thisMetatile, AreaPalettes[3, 0]);
+            }
+
+            for (byte _Palette = 1; _Palette < 4; _Palette++) {
+                for (byte Metatile = 0; Metatile < TileBeginnings[_Palette]; Metatile++) {
+                    for (byte _Tile = 0; _Tile < 4; _Tile++, peek++) {
+                        thisMetatile[_Tile] = GetTile(false, MetatilesRAW[peek]);
+                    }
+                    for (byte AreaType = 0; AreaType < 4; AreaType++) {
+                        Metatiles[(ushort)((AreaType << 8) | (_Palette << 6) | Metatile)] = BuildMetatile(thisMetatile, AreaPalettes[AreaType, _Palette]);
+                    }
+
+                    Metatiles[(ushort)((0x1000) | (_Palette << 6) | Metatile)] = BuildMetatile(thisMetatile, AreaPalettes[3, _Palette]);
+                }
+            }
+        }
+
+        void NormalizeScenery() {
+            ushort peek = 3;
+            for (byte BackgroundStyle = 0; BackgroundStyle < 3; BackgroundStyle ++) {
+                for (byte _BackgroundItem = 0; _BackgroundItem < 48; _BackgroundItem++, peek++) {
+                    BackSceneryData[BackgroundStyle, _BackgroundItem] = new BackgroundItem(SceneryRAW[peek]);
                 }
             }
 
-            TileBMP.Save("./output/" +Tile.ToString() + ".bmp");
+            for (byte Element = 0; Element < 12; Element++) {
+                byte[] thisElement = new byte[3];
+                for (byte _Metatile = 0; _Metatile < 3; _Metatile++, peek++) {
+                    thisElement[_Metatile] = SceneryRAW[peek];
+                }
+                BackSceneryMetatiles[Element] = new BackgroundElement(thisElement);
+            }
+        }
+
+        BuildMetatiles();
+
+        foreach (KeyValuePair<ushort, Bitmap> kvp in Metatiles) { 
+
+            kvp.Value.Save("./output/" + kvp.Key.ToString() + ".bmp");
         }
         
     }
@@ -53,6 +160,15 @@ public class Common {
         }
     }
 
+    public class Palette {
+        public Color[] Data = new Color[4];
+        public Palette(byte[] PaletteIDs, Color[] SYSPALETTE) {
+            for (byte _Color = 0; _Color < 4; _Color++) {
+                Data[_Color] = SYSPALETTE[PaletteIDs[_Color]];
+            }
+        }
+    }
+
     void ProcessCHR(ref Tile[,] ProcessedCHR, ref byte[] CHRRAW){
         for (ushort _Tile = 0; _Tile < 512; _Tile++) {
             ProcessedCHR[_Tile >> 8, _Tile & 0xff] = new Tile(ref CHRRAW, (ushort)(_Tile << 4));
@@ -60,15 +176,49 @@ public class Common {
     }
 
     public Tile GetTile(bool isOAM, byte ID){
-        return ProcessedCHR[isOAM ? 1 : 0, ID];
+        return ProcessedCHR[!isOAM ? 1 : 0, ID];
     }
 
     public Tile GetTile(bool isOAM, byte x, byte y){
-        return ProcessedCHR[isOAM ?  1 : 0, (y << 4) | x];
+        return ProcessedCHR[!isOAM ?  1 : 0, (y << 4) | x];
     }
 
     public Tile GetTile(ushort ID){
         return ProcessedCHR[ID >> 8, ID & 0xff];
+    }
+
+    public class BackgroundElement {
+        public byte[] Data;
+        public BackgroundElement(byte[] _Data) {
+            Data = _Data;
+        }
+    }
+
+    public class BackgroundItem {
+        public byte y, Element;
+        public BackgroundItem(byte Data) {
+            y = (byte)(Data >> 4);
+            Element = (byte)(Data & 0x0f);
+        }
+    }
+
+    public BackgroundElement GetBackground(byte BackgroundStyle, byte BackgroundCollumn) {
+        return BackSceneryMetatiles[BackSceneryData[BackgroundStyle, BackgroundCollumn].Element];
+    }
+
+    public Bitmap[] GetBackgroundMetatiles(byte BackgroundStyle, byte BackgroundCollumn, byte AreaType, byte AreaStyle, byte Foreground) {
+        Bitmap[] Return = new Bitmap[3];
+        if (Foreground == 0b111) {
+            for (byte _Metatile = 0; _Metatile < 3; _Metatile++) {
+                Return[_Metatile] = Metatiles[(ushort)((0x1000) | (BackSceneryMetatiles[BackSceneryData[BackgroundStyle, BackgroundCollumn].Element].Data[_Metatile]) & 0x3f)];
+            }
+        } else {
+            for (byte _Metatile = 0; _Metatile < 3; _Metatile++) {
+                byte thisMetatile = BackSceneryMetatiles[BackSceneryData[BackgroundStyle, BackgroundCollumn].Element].Data[_Metatile];
+                Return[_Metatile] = Metatiles[(ushort)(((thisMetatile & 0xc0) != 0 ? AreaStyle << 10 : 0) | (AreaType << 8) | thisMetatile)];
+            }
+        }
+        return Return;
     }
 
     public readonly static Color[] NESPALETTE = {
@@ -76,8 +226,9 @@ public class Common {
                         Color.FromArgb(0xff, 0x00, 0x1f, 0xb2), // dark blue    0x01
                         Color.FromArgb(0xff, 0x24, 0x04, 0xcb), // dark blue    0x02
                         Color.FromArgb(0xff, 0x52, 0x00, 0xb2), // purple       0x03
-                        Color.FromArgb(0xff, 0x80, 0x00, 0x24), // magenta      0x04 
-                        Color.FromArgb(0xff, 0x72, 0x0b, 0x00), // dark red     0x05 
+                        Color.FromArgb(0xff, 0x73, 0x00, 0x76), // purple       0x04
+                        Color.FromArgb(0xff, 0x80, 0x00, 0x24), // magenta      0x05 
+                        Color.FromArgb(0xff, 0x72, 0x0b, 0x00), // dark red     0x06
                         Color.FromArgb(0xff, 0x00, 0x52, 0x08), // brown        0x07
                         Color.FromArgb(0xff, 0x00, 0x24, 0x44), // gold         0x08
                         Color.FromArgb(0xff, 0x00, 0x57, 0x00), // dark green   0x09
